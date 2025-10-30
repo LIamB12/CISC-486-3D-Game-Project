fb6a56f319f3f400ff0f87a35147fac0e582db5a
LIamB12 <liam.beenken@gmail.com>
2025-10-30 00:33:23 -0400

    add character with basic animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,10 +10,22 @@ public class PlayerController : MonoBehaviour
     private float _rotationY;
     private float _verticalVelocity;
 
+    private Animator _animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        AnimatorStateInfo state = _animator.GetCurrentAnimatorStateInfo(0);
+        if (state.IsName("Swat") && state.normalizedTime >= 0.4f) {
+            _animator.SetBool("isAttacking", false);
+        }
     }
 
     public void Move(Vector2 movementVector) {
@@ -20,8 +33,16 @@ public class PlayerController : MonoBehaviour
         move = move * MovementSpeed * Time.deltaTime;
         _characterController.Move(move);
 
+        _animator.SetFloat("ForwardSpeed", movementVector.y);
+        _animator.SetFloat("HorizontalSpeed", movementVector.x);
+
         _verticalVelocity = _verticalVelocity + Gravity * Time.deltaTime;
         _characterController.Move(new Vector3(0, _verticalVelocity, 0) * Time.deltaTime);
+
+        if (_characterController.isGrounded && _animator.GetBool("isJumping"))
+        {
+            _animator.SetBool("isJumping", false);
+        }
     }
 
     public void Rotate(Vector2 rotationVector) {
@@ -29,9 +50,20 @@ public class PlayerController : MonoBehaviour
         transform.localRotation = Quaternion.Euler(0, _rotationY, 0);
     }
 
-    public void Jump() {
-        if (_characterController.isGrounded) {
+    public void Jump()
+    {
+        if (_characterController.isGrounded)
+        {
             _verticalVelocity = JumpForce;
+            _animator.SetBool("isJumping", true);
+        }
+    }
+    
+    public void Attack()
+    {
+        if (_characterController.isGrounded)
+        {
+            _animator.SetBool("isAttacking", true);
         }
     }
 }
